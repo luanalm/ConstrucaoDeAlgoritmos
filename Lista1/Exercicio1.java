2112b18b6ee9b791aee9782083668421d91f38a8
Luana Landulpho Martini <luana.landmartini@gmail.com>
2022-06-27 05:26:06 -0300

    Exercicio1 + anotações

--- Lista1/Exercicio1.java
+++ Lista1/Exercicio1.java
@@ -1,14 +1,14 @@
-import java.util.Scanner; //adicionado leitor de texto
+import java.util.Scanner; // adicionado leitor de texto
 
-public class Exercicio1 {
-  public static void main(String[] args) {
-    String nom;
-    Scanner ler = new Scanner(System.in);
+public class Exercicio1 { // arquivo a ser executado
+  public static void main(String[] args) { 
+    String nom; // definindo o nome da var string
+    Scanner ler = new Scanner(System.in); // comando para usar o leitor
 
-    System.out.print("Nome Completo: ");
-    nom =ler.nextLine();
+    System.out.print("Nome Completo: "); // print a ser respondido
+    nom = ler.nextLine(); // leitor registra o valor do string nom
 
-    System.out.println("Seu nome é " + nom);
+    System.out.println("Seu nome é " + nom); // mostrando o valor registrado pelo string nom
     }
 }
 
